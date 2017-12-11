bd9ec5320e2e14339effb1d32783ce2b6c7e3836
tarvi.liivak <tarvi.liivak@khk.ee>
2017-12-11 12:41:33 +0200

    Defineeritud ja väljastatud muutuja!

---
+++ võrdlused.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 11.12.2017
+ * Time: 12:38
+ */
+// defineeri 8 muutujat, mille väärtuseks
+// on täisarvus, reaalarvud, sõned ja boolean värtused
+$var1 = 5;
+$var2 = 5.0;
+$var3 = '5';
+$var4 = 2;
+$var5 = 2.0;
+$var6 = '2';
+$var7 = true;
+$var8 = false;
