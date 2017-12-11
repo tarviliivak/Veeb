128d4d4b482206d78da58da8832e7b69eac24e81
tarvi.liivak <tarvi.liivak@khk.ee>
2017-12-11 12:29:08 +0200

    Defineeritud ja väljastatud muutuja!

---
+++ Mina.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 11.12.2017
+ * Time: 12:11
+ */
+//üherealised kommentaarid
+/*
+ * mimte
+ * realine
+ * kommentaar
+ */
+/*
+ * defineeri oma nimega muutuja
+ * mille väärtuseks on sinu eesnimi
+ * ja väljasta antud muutuja väärtus
+ * ekraani koos lisatekstiga
+ * näiteks
+ * minu nimi on...
+ */
+
+$Tarvi = 'Tarvi';
+    //echo
+echo 'Minu nimi on'.$Tarvi.'<br />';
+//print funktsiooniga
+print 'Minu nimi on'.$Tarvi.'<br />';
+print ('Minu nimi on'.$Tarvi.'<br />');
+?>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Veeb
+#programmeerimisvahendid
+#PROGRAMMERIMISVAHENDID
