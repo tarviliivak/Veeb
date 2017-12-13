89a65bdf252cb0af578ec0a7d3d7a9dfcee2b0bf
tarvi.liivak <tarvi.liivak@khk.ee>
2017-12-13 09:05:49 +0200

    Kirjeldatud tingimuslausepõhistruktuur!

--- tingimuslaused.php
+++ tingimuslaused.php
@@ -13,3 +13,17 @@ if ($ilm == 'sajab') {
 }
 echo 'Väljas '.$ilm. '<br />';
 echo 'Minul on '.$tuju.' tuju<br />';
+//
+$vanus = rand(0, 100);
+if ($vanus >= 0 and $vanus < 11){
+    $staatus = 'laps';
+} elseif ($vanus > 10 and $vanus < 18){
+    $staatus = 'nooruk';
+} elseif ($vanus > 17 and $vanus < 65){
+    $staatus = 'täiskasvanu';
+} else {
+    $staatus = 'seenior';
+}
+
+echo 'Oled '.$vanus.' aastat vana.<br />';
+echo 'Oled '.$staatus.'<br />';
