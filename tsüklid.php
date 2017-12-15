5976f6adfb4deb679ab0a577a742bfc577df6da8
tarvi.liivak <tarvi.liivak@khk.ee>
2017-12-15 10:47:04 +0200

    Paaris ja paaritud arvud erinevates värvides

--- tsüklid.php
+++ tsüklid.php
@@ -5,6 +5,13 @@
  * Date: 15.12.2017
  * Time: 10:29
  */
+$varv = '';
 for($arv = 1; $arv <= 10; $arv++){
-    echo $arv.'<br />';
+    if ($arv % 2 == 0){
+        $varv = 'red';
+    }  else {
+        $varv = 'blue';
+    }
+    echo '<p style="color: '.$varv.' ">'.$arv.'<br />';
 }
+//täienda antud lahendust nii, et paaris arvud oleksid punased ja paaritud arvud sinised
