8c0e791cdc1909f56f153049274884d8684e25dc
tarvi.liivak <tarvi.liivak@khk.ee>
2017-12-18 15:21:44 +0200

    mitu korda esineb arv

--- funktsioonid2.php
+++ funktsioonid2.php
@@ -20,9 +20,35 @@ function arvusumma($number){
        $summa = $summa + $arv;
         $number = $number / 10;
          settype($number, 'integer');
-         echo 'number = '.$number.'<br />';
     }
     return $summa;
 }
-$number = 123;
+for ($kord = 1; $kord <= 5; $kord++){
+
+
+$number = rand(0,1000);
 echo 'Numbri '.$number.' arvude summa on '.arvusumma($number).'<br />';
+}
+
+//Koostada funktsioon nimega otsinumber,
+//mis võtab parameetrina suvalise arvu ja
+//kindlaks määratud arvu ja arvutab mitu korda määratud
+//arv esineb suvalises numbris, näiteks arv 5
+//esineb numbris 442158755745 neli korda
+//lahendamiseks ei tohi kasutada sõnetöötluse võimalusi
+//ja massiivtöötluse vahendeid
+//tulemus koos seletava tekstiga
+
+function otsi_arv($suvalinearv, $kindelarv){
+        $mitukorda = 0;
+    while ($suvalinearv != 0){
+        $arv = $suvalinearv % 10;
+       if ($arv == $kindelarv){
+           $mitukorda++;
+       }
+       $suvalinearv = $suvalinearv /10;
+       settype($suvalinearv, 'integer');
+    }
+echo ''.$mitukorda.'arv korda<br />';
+}
+otsi_arv(5645554564564,5);
