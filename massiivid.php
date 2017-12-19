ac2dc891d03d0b09f1477ee526260d0c7e339435
tarvi.liivak <tarvi.liivak@khk.ee>
2017-12-19 08:52:19 +0200

    defineeritud ja väljastatud testmassiiv!

---
+++ massiivid.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 19.12.2017
+ * Time: 8:44
+ */
+$arvud = array(5, 4, 3, 2, 1);
+var_dump($arvud);
+echo '<hr />';
+echo '<pre>';
+print_r($arvud);
+echo '</pre>';
