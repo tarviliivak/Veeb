9780a519e93be779de1403ecf6fede499f746c2f
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-17 10:47:00 +0200

    arvud teises järjekorras. table_test

--- OOP/tabel_test.php
+++ OOP/tabel_test.php
@@ -11,6 +11,7 @@ $minuTabel = new  tabel(array('a', 'b', 'c', 'd'));
 //väljastame tabeli objekti test kujul
 //
 $minuTabel->lisaRida(array(1, 2 , 3, 4 ));
+$minuTabel->lisaRidaPealkirjadega(array('d'=>1,'c'=> 2 ,'b'=> 3,'a'=> 4 ));
 $minuTabel->lisaRida(array(5, 6 , 7, 8 ));
 //
 echo '<pre>';
