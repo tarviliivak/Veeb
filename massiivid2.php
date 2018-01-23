0775145bdb2a44b7441d10e112263f7233f972cd
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:55:45 +0200

    Sisestatud massiivide töötluse alused

--- massiivid2.php
+++ massiivid2.php
@@ -11,18 +11,15 @@ $opilane = array(
     'vanus' => 15,
     'klass' => 9
 );
-echo '<pre />';
+echo '<pre>';
 print_r($opilane);
-echo '<pre />';
-
+echo '</pre>';
 echo $opilane['eesnimi'];
-
 echo '<hr />';
 foreach ($opilane as $voti=>$vaartus){
-    echo $voti. ' - '.$vaartus.'<br />';
+    echo $voti.' - '.$vaartus.'<br />';
 }
 echo '<hr />';
-
 $opilased = array(
     array(
         'eesnimi' => 'Mart',
@@ -32,110 +29,25 @@ $opilased = array(
     ),
     array(
         'eesnimi' => 'Kadri',
+        'perenimi' => 'Tamm',
+        'vanus' => 16,
+        'klass' => 10
+    ),
+    array(
+        'eesnimi' => 'Kadi',
         'perenimi' => 'Kuusk',
         'vanus' => 15,
         'klass' => 9
-    ),
-    array(
-        'eesnimi' => 'Mart',
-        'perenimi' => 'Mets',
-        'vanus' => 15,
-        'klass' => 9
     )
 );
-echo '<pre />';
+echo '<pre>';
 print_r($opilased);
-echo '<pre />';
-
+echo '</pre>';
 foreach ($opilased as $opilane){
     foreach ($opilane as $element=>$vaartus){
-        echo $element. ' - '.$vaartus.'<br />';
+        echo $element.' - '.$vaartus.'<br />';
     }
     echo '--------------'.'<br />';
 }
-//Loo funktsioon nimega OtsiRaamat, mille parameetrina on raamatute massiiv
-//ja seisun, funktsioon tagastab massiivi,
-//kus on raamatud vastava seisundiga(laenutatud või mitte)
-//Loo funktsioon nimega raamatuTabel, mis võtab parameetrina massiivi ja väljastab
-// tulemused tabelina. Pealkirjas massiivi võtmed.
-//
-//
-
-function raamatuteTabel($raamatud){
-    echo '<table border "1">';
-    $pealkiriValjastatud = false;
-    foreach ($raamatud as $raamat){
-        echo '<tr>';
-        if (!$pealkiriValjastatud){
-            $pealkiri = array_keys($raamat);
-            foreach ($pealkiri as $nimetus){
-                echo '<th>';
-                echo $nimetus;
-                echo '</th>';
-            }
-            echo '</tr>';
-            $pealkiriValjastatud = true;
-        }
-        foreach ($raamat as $element =>$vaartus){
-            echo '<td>';
-            echo $vaartus;
-            echo '</td>';
-        }
-        echo '</tr>';
-    }
-    echo '</table>';
-}
-
-function otsiRaamat($raamatud, $status){
-    $leitudraamatud = array();
-    foreach ($raamatud as $raamat){
-        if ($raamat['status'] == $status){
-            $leitudraamatud[] = $raamat;
-        }
-    }
-    return $leitudraamatud;
-}
-
-
-
-
-$raamatud = array(
-    array(
-        'title' => 'Peppa Goes To London',
-        'author' => 'Ladybird',
-        'print' => 'Penguin',
-        'status' => 'valjas'
-    ),
-    array(
-        'title' => 'Toto, Joonas ja püha Mimoos',
-        'author' => 'Kristiina K.',
-        'print' => 'Heli Kirjastus OÜ',
-        'status' => 'sees'
-    ),
-    array(
-        'title' => 'Lugusid loomadest',
-        'author' => 'Ernest Thompson Seton',
-        'print' => 'Ernest Thompson Seton',
-        'status' => 'sees'
-    ),
-    array(
-        'title' => 'Miljon miksi ja miljon vastust',
-        'author' => 'Mina',
-        'print' => 'TEA Kirjastus',
-        'status' => 'sees'
-    ),
-    array(
-        'title' => 'Teadmiste Puu: Maailma Loodus',
-        'author' => 'AJ Wood, Mike Jolley',
-        'print' => 'Pikoprint',
-        'status' => 'valjas'
-    )
-);
-
-$kohalRaamatud = otsiRaamat($raamatud,  'sees');
-raamatuteTabel($kohalRaamatud);
-
-raamatuteTabel($raamatud);
-
 //
 //
