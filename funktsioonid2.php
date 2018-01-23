df2e85ac9616a2093b75c3fb0c3ae4cf52ce530f
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:33:01 +0200

    Kontrollitud arvude eraldamisalgoritm

--- funktsioonid2.php
+++ funktsioonid2.php
@@ -4,51 +4,25 @@
  * User: tarvi.liivak
  * Date: 18.12.2017
  * Time: 14:11
- */
-//Koostada funktsioon nimega arvusumma, mis võtab parameetrina
-//suvalise numbri ja arvutab
-//numbri arvude summa - juhul kui on argumendina
-//funktsioonile antud number 123, siis programm peab leidma 1+2+3 summa, ehk 6
-//Ei tohi kasutada sõnetöötluse võimalusi
-//Funktsioon peab tagastama leitud summa põhiprogrammile
-//Põhiprogrammis peab kasutama väljatrükis tagastatud väärtus koos selgitava tekstiga
-
-function arvusumma($number){
+/*
+ * 1. Koostada funktsioon nimega arvuSumma, mis võtab
+ * parameetrina suvalise numbri ja arvutab numbri
+ * arvude summat - juhul, kui on argumendina
+ * funktsioonile antud number 123, siis programm
+ * peab leidma 1+2+3 summa, ehk 6.
+ * Lahendamiseks ei tohi kasutada sõnetöötluse võimalused.
+ * Funktsioon peab tagastama leitud summa põhiprogrammile.
+ * Põhiprogrammis kasutada väljatrükis tagastatud väärtus koos
+ * selgitava tekstiga
+ * */
+function arvuSumma($number){
     $summa = 0;
-    while ($number != 0){
+    while($number != 0){
         $arv = $number % 10;
-       $summa = $summa + $arv;
+        echo 'arv = '.$arv.'<br />';
         $number = $number / 10;
-         settype($number, 'integer');
+        settype($number, 'integer');
+        echo 'number = '.$number.'<br />';
     }
-    return $summa;
 }
-for ($kord = 1; $kord <= 5; $kord++){
-
-
-$number = rand(0,1000);
-echo 'Numbri '.$number.' arvude summa on '.arvusumma($number).'<br />';
-}
-
-//Koostada funktsioon nimega otsinumber,
-//mis võtab parameetrina suvalise arvu ja
-//kindlaks määratud arvu ja arvutab mitu korda määratud
-//arv esineb suvalises numbris, näiteks arv 5
-//esineb numbris 442158755745 neli korda
-//lahendamiseks ei tohi kasutada sõnetöötluse võimalusi
-//ja massiivtöötluse vahendeid
-//tulemus koos seletava tekstiga
-
-function otsi_arv($suvalinearv, $kindelarv){
-        $mitukorda = 0;
-    while ($suvalinearv != 0){
-        $arv = $suvalinearv % 10;
-       if ($arv == $kindelarv){
-           $mitukorda++;
-       }
-       $suvalinearv = $suvalinearv /10;
-       settype($suvalinearv, 'integer');
-    }
-echo ''.$mitukorda.'arv korda<br />';
-}
-otsi_arv(5645554564564,5);
+arvuSumma(1234);
