52e43fb56a3a76881dcecccb100ab2cbbe7246a9
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:35:21 +0200

    Arvutatud ja tagastatud numbri arvude summa

--- funktsioonid2.php
+++ funktsioonid2.php
@@ -19,10 +19,11 @@ function arvuSumma($number){
     $summa = 0;
     while($number != 0){
         $arv = $number % 10;
-        echo 'arv = '.$arv.'<br />';
+        $summa = $summa + $arv;
         $number = $number / 10;
         settype($number, 'integer');
-        echo 'number = '.$number.'<br />';
     }
+    return $summa;
 }
-arvuSumma(1234);
+$number = 123;
+echo 'Numbri '.$number.' arvude summa on '.arvuSumma($number).'<br />';
