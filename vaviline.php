4ffed18cf58105e7ee61150d3d031ea09f20862f
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:20:11 +0200

    Koostatud värvilise teksti väljastamine

---
+++ vaviline.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 23.01.2018
+ * Time: 15:11
+ */
+for($rida = 1; $rida <= 5; $rida++){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $juhuTaisarv = rand(0, 15);
+        $juhuHex = dechex($juhuTaisarv);
+        $varv = $varv.$juhuHex;
+    }
+    echo '<font color="'.$varv.'">Värviline tekst</font><br />';
+}
--- whiletsükkel.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tarvi.liivak
- * Date: 23.01.2018
- * Time: 15:11
- */
