aa5a205847dd85e53ea9084daac95ff46c240b07
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:43:23 +0200

    Loodud juhuarvudest koosnev massiiv ja teostatud testväljastus

--- massiivid.php
+++ massiivid.php
@@ -19,3 +19,29 @@ echo 'Massiiv $arvud koosneb '.count($arvud).' elemendist<br />';
 for($i = 0; $i < count($arvud); $i++){
     echo $arvud[$i].'<br />';
 }
+echo '<hr />';
+foreach ($arvud as $arv){
+    echo '<i>'.$arv.'</i><br />';
+}
+echo '<hr />';
+/*
+ * Koosta funktsioon nimega looMassiiv,
+ * mis võtab parameetrina elementide
+ * arvu ja loob juhuarvudest (täisarvud)
+ * vahemikust 0-99 koosneva massiivi.
+ * Loodud massiiv tuleb tagastada
+ * põhiprogrammile ja kontrollida sisu
+ * testväljastuse abil
+ * */
+function looMassiiv($elementideArv){
+    $massiiv = array();
+    for($i = 0; $i < $elementideArv; $i++){
+        $juhuarv = rand(0, 99);
+        $massiiv[] = $juhuarv;
+    }
+    return $massiiv;
+}
+$arvudeMassiiv = looMassiiv(8);
+echo '<pre>';
+print_r($arvudeMassiiv);
+echo '</pre>';}
