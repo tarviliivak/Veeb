e1c1c7316fbebf6b5e76d743ff83c39a5c17f9ff
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:46:28 +0200

    Koostatud 2D massiivi loomine

--- massiivid.php
+++ massiivid.php
@@ -65,4 +65,26 @@ function valjastaMassiiv($massiiv){
     echo '</table>';
 }
 valjastaMassiiv($arvudeMassiiv);
-© 2018 GitHub, Inc.
+echo '<hr />';
+/*
+ * Muuda looMassiiv funktsiooni nii,
+ * et ma võtaks parameetritena ridade ja
+ * veergude arvu ja looks 2D massiivi
+ * ning tagastaks põhiprogrammile
+ * Funktsiooni nimeks paneme
+ * loo2DMassiiv*/
+function loo2DMassiiv($ridadeArv, $veergudeArv){
+    $massiiv2D = array();
+    for($reaNumber = 0; $reaNumber < $ridadeArv; $reaNumber++){
+        $massiiv2D[] = array();
+        for ($veeruNumber = 0; $veeruNumber < $veergudeArv; $veeruNumber++){
+            $juhuarv = rand(0,99);
+            $massiiv2D[$reaNumber][] = $juhuarv;
+        }
+    }
+    return $massiiv2D;
+}
+$arvudeMassiiv2D = loo2DMassiiv(2, 3);
+echo '<pre>';
+print_r($arvudeMassiiv2D);
+echo '</pre>';
