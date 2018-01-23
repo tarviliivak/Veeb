63395e90243e8945e7049acb2bdc8c76094c63dd
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:31:03 +0200

    Loodud teksti tabel kasutades muutujate dünaamilist genereerimist

--- tabel.php
+++ tabel.php
@@ -4,11 +4,12 @@
  * User: tarvi.liivak
  * Date: 23.01.2018
  * Time: 15:27
- */* Koosta funktsioon nimega valjastaTabel,
+/*
+ * Koosta funktsioon nimega valjastaTabel,
  * mis võtab parameetritena tabeli ridade ja
-* veergude arv ning väljastab vastava suurusega
-* tabel, mille pesad on täidetud
-* juhuarvudega vahemikus 10-99*/
+ * veergude arv ning väljastab vastava suurusega
+ * tabel, mille pesad on täidetud
+ * juhuarvudega vahemikus 10-99*/
 /*
  * Loo funktsioon genereeriVarv mis genereerib ja tagastab
  * põhiprogrammile genereeritud värv kujul #XXXXXX
@@ -41,3 +42,25 @@ function valjastaTabel($ridadeArv, $veergudeArv){
     echo '</table>';
 }
 valjastaTabel(5, 7);
+/*
+ * Koosta funktsioon nimega tekstiTabel, mis võtab parameetrina
+ * 4 sõna ja väljastab tabelit, mis koosneb
+ * 4-st reas, iga rida sisaldab üks sõna, mis on parameetrina määratud
+ *
+ * Lahenduses ei tohi kasutada koodi dubleerimist! - st. ei ole võimalik
+ * 4 korda kirjutada sama kood, vaid tuleb mõelda,
+ * kuidas saab tsüklite ja muutujate defineerimisega lahendada antud
+ * ülesanne
+ * */
+function tekstiTabel($sona1, $sona2, $sona3, $sona4){
+    echo '<table border="1">';
+    for($reaNumber = 1; $reaNumber <= 4; $reaNumber++){
+        echo '<tr>';
+        echo '<td>';
+        echo ${'sona'.$reaNumber};
+        echo '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+tekstiTabel('See', 'on', 'minu', 'tabel');
