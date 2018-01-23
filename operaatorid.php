5e2d2fbebfc72972e2364c63e653fcb4383b8353
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 14:51:53 +0200

    Defineeritud ja katsetatud erinevad operaatorid

---
+++ operaatorid.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 23.01.2018
+ * Time: 14:51
+ */
+// defineeri 8 muutujat, mille väärtuseks
+// on täisarvus, reaalarvud, sõned ja boolean värtused
+$var1 = 5;
+$var2 = 5.0;
+$var3 = '5';
+$var4 = 2;
+$var5 = 2.0;
+$var6 = '2';
+$var7 = true;
+$var8 = false;
