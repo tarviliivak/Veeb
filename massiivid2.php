8d49b54eb1010419b5c75ba448249b1f565a9ee9
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:57:59 +0200

    Raamatute massiiv testandmetega

--- massiivid2.php
+++ massiivid2.php
@@ -69,3 +69,35 @@ foreach ($opilased as $opilane){
  * väljastab tulemused tabeli kujul. Tabeli pealkirjaks
  * on massiivi võtmed (pealkirja loomiseks võib kasutada eraldi funktsiooni)
  * */
+$raamatud = array(
+    array(
+        'title' => 'Peppa Goes To London',
+        'author' => 'Ladybird',
+        'print' => 'Penguin',
+        'status' => 'valjas'
+    ),
+    array(
+        'title' => 'Toto, Joonas ja püha Mimoos',
+        'author' => 'Kristiina K.',
+        'print' => 'Heli Kirjastus OÜ',
+        'status' => 'sees'
+    ),
+    array(
+        'title' => 'Lugusid loomadest',
+        'author' => 'Ernest Thompson Seton',
+        'print' => 'Ernest Thompson Seton',
+        'status' => 'sees'
+    ),
+    array(
+        'title' => 'Miljon miksi ja miljon vastust',
+        'author' => '',
+        'print' => 'TEA Kirjastus',
+        'status' => 'sees'
+    ),
+    array(
+        'title' => 'Teadmiste Puu: Maailma Loodus',
+        'author' => 'AJ Wood, Mike Jolley',
+        'print' => 'Pikoprint',
+        'status' => 'valjas'
+    )
+);
