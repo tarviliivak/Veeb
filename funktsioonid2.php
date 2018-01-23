38f9096e5f6b94440dee6334f47332baca2ed507
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 15:36:44 +0200

    Arvutatud kindla arvu esinemist suvalises arvus

--- funktsioonid2.php
+++ funktsioonid2.php
@@ -25,5 +25,34 @@ function arvuSumma($number){
     }
     return $summa;
 }
-$number = 123;
-echo 'Numbri '.$number.' arvude summa on '.arvuSumma($number).'<br />';
+for ($kord = 1; $kord <= 5; $kord++){
+    $number = rand(0, 1000);
+    echo 'Numbri '.$number.' arvude summa on '.arvuSumma($number).'<br />';
+}
+/*
+ * 2. Koostada funktsioon nimega otsiNumber,
+ * mis võtab parameetrina suvalise arvu ja
+ * kindlaks määratud arvu ja arvutab mitu korda määratud
+ * arv esineb suvalises numbris, näiteks arv 5
+ * esineb numbris 442158755745 neli korda.
+ * Lahendamiseks ei tohi kasutada sõnetöötluse võimalused
+ *
+ * Funktsioon peab väljastama tulemus koos seletava tekstiga
+*/
+function otsiNumber($suvalineArv, $kindelArv){
+    echo 'Arv '.$kindelArv.' esineb arvus '.$suvalineArv;
+    $mituKorda = 0;
+    while($suvalineArv != 0){
+        $arv = $suvalineArv % 10;
+        if($arv == $kindelArv){
+            $mituKorda++;
+        }
+        $suvalineArv = $suvalineArv / 10;
+        settype($suvalineArv, 'integer');
+    }
+    echo ' '.$mituKorda.' arv korda<br />';
+}
+otsiNumber(442158755745, 5);
+otsiNumber(442158755745, 4);
+otsiNumber(442158755745, 7);
+otsiNumber(442158755745, 8);
