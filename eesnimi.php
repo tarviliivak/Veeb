4880a5fbe03d67bbb310efcfdab4840a6877ed56
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 14:49:37 +0200

    Defineeritud ja väljastatud eenimi

---
+++ eesnimi.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 23.01.2018
+ * Time: 14:48
+ */
+//üherealised kommentaarid
+/*
+ * mimte
+ * realine
+ * kommentaar
+ */
+/*
+ * defineeri oma nimega muutuja
+ * mille väärtuseks on sinu eesnimi
+ * ja väljasta antud muutuja väärtus
+ * ekraani koos lisatekstiga
+ * näiteks
+ * minu nimi on...
+ */
+$Tarvi = 'Tarvi';
+//echo
+echo 'Minu nimi on'.$Tarvi.'<br />';
+//print funktsiooniga
+print 'Minu nimi on'.$Tarvi.'<br />';
+print ('Minu nimi on'.$Tarvi.'<br />');
+?>
